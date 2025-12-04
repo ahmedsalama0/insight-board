ccb9f4ac57c00753616b1ff66cb5b0d4530e2679
ahmedsalama0 <130906715+ahmedsalama0@users.noreply.github.com>
2025-12-04 12:52:44 +0200

    bugs

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -40,7 +40,10 @@ function KanbanBoard() {
   );
 
   return (
-    <div style={{ display: 'flex', gap: '20px', backgroundColor: '#999' }}>
+    <div
+      style={{ display: 'flex', gap: '20px', backgroundColor: '#999' }}
+      className="border border-2 h-full"
+    >
       {/* we added the sensor to activate the delete button
       since it is not working as the dndContext can't differentiate between delete click and drag click
       */}
