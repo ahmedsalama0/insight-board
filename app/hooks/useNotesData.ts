e93dedd4b7053c267470f4cb557ef522e2133f06
ahmedsalama0 <130906715+ahmedsalama0@users.noreply.github.com>
2025-12-08 14:31:19 +0200

    snapshot

--- app/hooks/useNotesData.ts
+++ app/hooks/useNotesData.ts
@@ -1,14 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { BASE_URL } from '../global/constants';
 import axios from 'axios';
+import { Note } from '../tasks/models/types.model';
+
+import { createNote as generateNote } from '../utilities/helpers';
+
+const QUERY_KEY = 'notes';
+const NOTES_ENDPOINT = 'notes';
+
 //Read notes
 const fetchNotes = () => {
-  return axios.get(`${BASE_URL}/notes`);
+  return axios.get(`${BASE_URL}/${NOTES_ENDPOINT}`);
 };
 
 export const useNotesFetchData = () => {
   return useQuery({
-    queryKey: ['notes'],
+    queryKey: [QUERY_KEY],
     queryFn: fetchNotes,
     //staleTime: 1000,
     //refetchOnWindowFocus: 'always',
@@ -16,5 +23,36 @@ export const useNotesFetchData = () => {
   });
 };
 
-const createNotes = () => {};
-export const useNotesCreateData = () => {};
+//CREATE a NOTE
+const createNote = () => {
+  const note = generateNote();
+  return axios.post(`${BASE_URL}/${NOTES_ENDPOINT}`, note);
+};
+export const useNotesCreateData = () => {
+  return useMutation({
+    mutationFn: () => {
+      return createNote();
+    },
+
+    onMutate: async (newNote, context) => {
+      await context.client.cancelQueries({
+        queryKey: [QUERY_KEY],
+      });
+
+      const previousData = context.client.getQueryData([QUERY_KEY]);
+
+      if (previousData) {
+        context.client.setQueryData([QUERY_KEY], {
+          ...previousData,
+          data: [...previousData?.data, newNote],
+        });
+      }
+    },
+    onError: (err, newTask, onMutateResult, context) => {
+      context.client.setQueryData([QUERY_KEY], onMutateResult?.previousData);
+    },
+    onSettled: (data, error, variables, onMutateResult, context) => {
+      return context.client.invalidateQueries({ queryKey: [QUERY_KEY] });
+    },
+  });
+};
---
+++ app/utilities/helpers.ts
@@ -0,0 +1,9 @@
+import { Note } from '../tasks/models/types.model';
+import { generateId } from '../tasks/page';
+
+export function createNote(): Note {
+  return {
+    id: generateId(),
+    content: 'untitled',
+  };
+}
